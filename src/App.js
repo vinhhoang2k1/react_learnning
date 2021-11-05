8520bfd03178deca02572dac4bf8086ea25db234
HOÀNG ĐỨC VINH <20200120@gmail.com>
2021-11-05 17:56:19 +0700

    thu sau

--- src/App.js
+++ src/App.js
@@ -3,45 +3,20 @@ import { useState } from "react"
 
 
 function App() {
-  // const array = [100, 200, 300]
 
-  // const [counter, setCount] = useState(() => {
-  //   const total = array.reduce((total, cur) => total + cur)
-  //   console.log(total);
-  //   return total;
-  // });
-  // // counter duoc gan gia tri khoi tao 
-  // // setCounter la 1 ham co chuc nang set lai gia tri
-  // // doi so cua ham useState(initial_value) va initial_value chi duoc goi lai 1 lan nhu vi du tren
-  // const handleIncrease = () => {
-  //   setCount(() => counter + 1)
-  // }
-  // return (
-  //   <div style={{ padding: 20 }}>
-  //     <h2>{counter}</h2>
-  //     <button onClick={handleIncrease} >increase</button>
-  //   </div>
-  // )
+  const [posts, setPost] = useState([])
+fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(res => res.json())
+    // .then(posts => {
+    //   setPost(posts)
+      
+    // })
 
-  const [sv, setSv] = useState({
-    nameSv: 'vinh',
-    address: 'hai van'
-  })
+  return(
+    <div >
+      <h2></h2>
+    </div>
+  )
 
-  const handleSv = () => {
-    setSv({
-      ...sv,
-      bio: 'hello world'
-    })
-  }
-  return (
-      <div style={{ padding: 20 }}>
-        <h2>{JSON.stringify(sv)}</h2>
-        <button onClick={handleSv} >increase</button>
-      </div>
-    )
 }
-
-
-
 export default App;
---
+++ src/content.js
@@ -0,0 +1,8 @@
+
+
+function Content() {
+    return (
+        <h2>hello world</h2>
+    )
+}
+export default Content;
