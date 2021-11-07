69eb37897ad228dc123c000ab903a1cf5016973c
canhht <canhht@scuti.asia>
2021-11-07 19:58:11 +0700

    chu nhat

--- src/App.js
+++ src/App.js
@@ -1,22 +1,77 @@
-import { useState } from "react"
-
-
+import { useEffect, useState } from "react";
 
 function App() {
+  const tabs = ["posts", "comments", "albums"];
+  const [posts, setPost] = useState([]);
+  const [type, setType] = useState("posts");
+  const [goToTop, setGoToTop] = useState(false);
+  // console.log(posts);
 
-  const [posts, setPost] = useState([])
-fetch('https://jsonplaceholder.typicode.com/posts')
-    .then(res => res.json())
-    // .then(posts => {
-    //   setPost(posts)
-      
-    // })
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/${type}`)
+      .then((res) => res.json())
+      .then((posts) => {
+        setPost(posts);
+      });
+  }, [type]);
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY >= 200) {
+        setGoToTop(true);
+      } else {
+        setGoToTop(false);
+      }
+    };
+    window.addEventListener("scroll", handleScroll);
+    return (
+      // window.removeEventListener("scroll", handleScroll)
+      console.log("remove listen")
 
-  return(
-    <div >
-      <h2></h2>
+    )
+  }, []);
+
+  return (
+    <div>
+      {/* {console.log(posts)} */}
+      {tabs.map((tab) => {
+        return (
+          <>
+            <button
+              key={tab}
+              style={
+                type === tab
+                  ? {
+                      background: "#333",
+                      color: "#fff",
+                    }
+                  : {}
+              }
+              onClick={() => setType(tab)}
+            >
+              {tab}
+            </button>
+          </>
+        );
+      })}
+      <ul>
+        {posts.map((post) => {
+          return <li key={post.id}>{post.title || post.id}</li>;
+        })}
+      </ul>
+      {goToTop && (
+        <button
+          key={goToTop}
+          style={{
+            position: "fixed",
+            padding: 10,
+            bottom: 20,
+            right: 20
+          }}
+        >
+          Go To Top
+        </button>
+      )}
     </div>
-  )
-
+  );
 }
 export default App;
--- src/content.js
+++ src/content.js
@@ -1,8 +1,25 @@
-
+import { useEffect, useState } from "react";
 
 function Content() {
-    return (
-        <h2>hello world</h2>
-    )
+  const [avata, setAvata] = useState();
+
+  useEffect(() => {
+    return () => {
+      avata && URL.revokeObjectURL(avata.preview);
+    };
+  }, [avata]);
+
+  const chooseFile = (e) => {
+    const file = e.target.files[0];
+    file.preview = URL.createObjectURL(file);
+    setAvata(file);
+  };
+
+  return (
+    <>
+      <input type="file" onChange={chooseFile} />
+      {avata && <img src={avata.preview} alt="" width="80%"></img>}
+    </>
+  );
 }
 export default Content;
